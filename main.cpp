90907779f124ed95caf375b47e49034fc19af21e
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-06-10 10:38:42 +0200

    Created SDL2 window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const int VIEW_WIDTH = 320;
+    const int VIEW_HEIGHT = 240;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("Roguelike", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, VIEW_WIDTH, VIEW_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Delay(2000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
