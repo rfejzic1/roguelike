ecf1e8090269a7d895a7df1a7419bcb58e2cf022
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-06-15 10:28:26 +0200

    Defined addition and subtraction on Vector2D

--- src/core/Vector2D.h
+++ src/core/Vector2D.h
@@ -6,4 +6,31 @@ struct Vector2D {
 
     Vector2D() = default;
     Vector2D(int x, int y) : x(x), y(y) {}
+
+    Vector2D& operator +=(const Vector2D& other) {
+        x += other.x;
+        y += other.y;
+
+        return *this;
+    }
+
+    Vector2D& operator -=(const Vector2D& other) {
+        *this += (-other);
+        return *this;
+    }
+
+    Vector2D operator +(const Vector2D& other) const {
+        return {
+            x + other.x,
+            y + other.y
+        };
+    }
+
+    Vector2D operator -() const {
+        return { -x, -y };
+    }
+
+    Vector2D operator -(const Vector2D& other) const {
+        return *this + (-other);
+    }
 };
