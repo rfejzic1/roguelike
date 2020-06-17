13845c5541c79c39a105121625f62e4237bf99bc
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-06-17 07:44:24 +0200

    Implemented Matrix copy/move operations

--- src/Matrix.h
+++ src/Matrix.h
@@ -6,12 +6,15 @@ class Matrix {
     int rows;
     int cols;
     T** matrix;
+
+    T** allocate(int numOfRows, int numOfCols, T defaultValue);
+    void deallocate();
 public:
     Matrix(int rows, int cols, T defaultValue = T());
-    Matrix(const Matrix& other) = delete;
-    Matrix(Matrix&& other) = delete;
-    Matrix& operator =(const Matrix& other) = delete;
-    Matrix& operator =(Matrix&& other) = delete;
+    Matrix(const Matrix<T>& other);
+    Matrix(Matrix<T>&& other);
+    Matrix<T>& operator =(const Matrix<T>& other);
+    Matrix<T>& operator =(Matrix<T>&& other);
     T get(int row, int col);
     void set(int row, int col, const T& value);
     ~Matrix();
@@ -21,26 +24,12 @@ template<typename T>
 Matrix<T>::Matrix(int rows, int cols, T defaultValue)
     : rows(rows), cols(cols), matrix(nullptr)
 {
-    try{
-        matrix = new T*[rows] {nullptr};
-        for(int i = 0; i < rows; i++) {
-            matrix[i] = new T[cols];
-            std::fill(matrix[i], matrix[i] + cols, defaultValue);
-        }
-    } catch(std::bad_alloc& err) {
-        for(int i = 0; i < rows; i++) {
-            delete[] matrix[i];
-        }
-        delete[] matrix;
-    }
+    matrix = allocate(rows, cols, defaultValue);
 }
 
 template<typename T>
 Matrix<T>::~Matrix() {
-    for(int i = 0; i < rows; i++) {
-        delete[] matrix[i];
-    }
-    delete[] matrix;
+    deallocate();
 }
 
 template<typename T>
@@ -51,4 +40,90 @@ T Matrix<T>::get(int row, int col) {
 template<typename T>
 void Matrix<T>::set(int row, int col, const T &value) {
     matrix[row][col] = value;
+}
+
+template<typename T>
+Matrix<T>::Matrix(const Matrix<T> &other) {
+    matrix = allocate(other.rows, other.cols, T());
+    rows = other.rows;
+    cols = other.cols;
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            matrix[i][j] = other.matrix[i][j];
+        }
+    }
+}
+
+template<typename T>
+Matrix<T>::Matrix(Matrix<T> &&other) {
+    rows = other.rows;
+    cols = other.cols;
+    matrix = other.matrix;
+    other.rows = 0;
+    other.cols = 0;
+    other.matrix = nullptr;
+}
+
+template<typename T>
+Matrix<T> &Matrix<T>::operator=(const Matrix<T> &other) {
+    if(&other == this) {
+        return *this;
+    }
+
+    deallocate();
+    matrix = allocate(other.rows, other.cols, T());
+    rows = other.rows;
+    cols = other.cols;
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            matrix[i][j] = other.matrix[i][j];
+        }
+    }
+
+    return *this;
+}
+
+template<typename T>
+Matrix<T> &Matrix<T>::operator=(Matrix<T> &&other) {
+    if(&other == this) {
+        return *this;
+    }
+
+    rows = other.rows;
+    cols = other.cols;
+    matrix = other.matrix;
+    other.rows = 0;
+    other.cols = 0;
+    other.matrix = nullptr;
+
+    return *this;
+}
+
+template<typename T>
+T **Matrix<T>::allocate(int numOfRows, int numOfCols, T defaultValue) {
+    T** mat = nullptr;
+    try{
+        mat = new T*[numOfRows] {nullptr};
+        for(int i = 0; i < numOfRows; i++) {
+            mat[i] = new T[numOfCols];
+            std::fill(mat[i], mat[i] + numOfCols, defaultValue);
+        }
+    } catch(std::bad_alloc& err) {
+        for(int i = 0; i < numOfRows; i++) {
+            delete[] mat[i];
+        }
+        delete[] mat;
+    }
+    return mat;
+}
+
+template<typename T>
+void Matrix<T>::deallocate() {
+    for(int i = 0; i < rows; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+    matrix = nullptr;
 }
